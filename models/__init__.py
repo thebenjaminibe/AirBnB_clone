6a11840682bcb2144c87bd76a010f74532a38c9c
thebenjaminibe <frankynoble@gmail.com>
2022-08-07 20:45:43 -0700

    commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,16 @@
-#!/usr/bin/env python3
-"""Initialize the storage engine"""
+#!/usr/bin/python3
+"""
+Instantiation file that starts the file storage system
+"""
+from models.engine import file_storage
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
 
-from .engine.file_storage import FileStorage
 
-storage = FileStorage()
+storage = file_storage.FileStorage()
 storage.reload()
